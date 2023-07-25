d322811cd3d520767c330ca47896058dff8e37d9
Md Abdullah Al Masum <52232643+masum029@users.noreply.github.com>
2023-07-25 15:09:21 +0600

    New Assignment added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routers, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import Header from './components/Header';
 import IncomePage from './pages/IncomePage';
@@ -28,107 +28,14 @@ const App = () => {
           </Navbar.Collapse>
         </Navbar>
 
-        <Routers>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route path="/income">
-            <IncomePage />
-          </Route>
-          <Route path="/expense">
-            <ExpensePage />
-          </Route>
-        </Routers>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/income" element={<IncomePage />} />
+          <Route path="/expense" element={<ExpensePage />} />
+        </Routes>
       </div>
     </Router>
   );
 };
 
 export default App;
-
-
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-// import IncomePage from "./Component/IncomePage";
-// import ExpensePage from "./Component/ExpensePage";
-// import Dashboard from "./Component/Dashboard";
- 
-
-// function App() {
-//   return (
-// <Router>
-// <div className="container mt-4">
-// <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//           <div className="collapse navbar-collapse">
-//             <ul className="navbar-nav">
-//             <li className="nav-item">
-//                 <Link className="nav-link" to="/dashboard">Dashboard</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/income">Income</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/expenses">Expenses</Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </nav>
-
-//         <Routes>
-//           <Route path="/dashboard" element={<Dashboard />}/>
-//           <Route path="/income" element={<IncomePage />}/>
-//           <Route path="/expenses" element={<ExpensePage />}/>
-//         </Routes>
-// </div>
-
-// </Router>
-   
-//   );
-// }
-
-// export default App;
-
-
-
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-// import IncomePage from "./Component/IncomePage";
-// import ExpensePage from "./Component/ExpensePage";
-// import Dashboard from "./Component/Dashboard";
- 
-
-// function App() {
-//   return (
-// <Router>
-// <div className="container mt-4">
-// <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//           <div className="collapse navbar-collapse">
-//             <ul className="navbar-nav">
-//             <li className="nav-item">
-//                 <Link className="nav-link" to="/dashboard">Dashboard</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/income">Income</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link" to="/expenses">Expenses</Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </nav>
-
-//         <Routes>
-//           <Route path="/dashboard" element={<Dashboard />}/>
-//           <Route path="/income" element={<IncomePage />}/>
-//           <Route path="/expenses" element={<ExpensePage />}/>
-//         </Routes>
-// </div>
-
-// </Router>
-   
-//   );
-// }
-
-// export default App;
-
-
